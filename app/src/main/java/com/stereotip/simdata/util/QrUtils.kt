98dc093282d6f9789b3c353a379d5b37e08dca82
s035785178-stack <s035785178@gmail.com>
2026-03-17 03:40:50 +0200

    Update QrUtils.kt

--- app/src/main/java/com/stereotip/simdata/util/QrUtils.kt
+++ app/src/main/java/com/stereotip/simdata/util/QrUtils.kt
@@ -1,25 +1,40 @@
-package com.stereotip.simdata.util
+<?xml version="1.0" encoding="utf-8"?>
+<manifest xmlns:android="http://schemas.android.com/apk/res/android">
 
-import android.graphics.Bitmap
-import android.graphics.Color
-import com.google.zxing.BarcodeFormat
-import com.google.zxing.EncodeHintType
-import com.google.zxing.MultiFormatWriter
-import com.google.zxing.common.BitMatrix
+    <!-- הרשאות -->
+    <uses-permission android:name="android.permission.RECEIVE_SMS"/>
+    <uses-permission android:name="android.permission.READ_SMS"/>
+    <uses-permission android:name="android.permission.READ_PHONE_STATE"/>
+    <uses-permission android:name="android.permission.READ_PHONE_NUMBERS"/>
+    <uses-permission android:name="android.permission.CALL_PHONE"/>
 
-object QrUtils {
-    fun createQrBitmap(content: String, size: Int = 900): Bitmap {
-        val hints = hashMapOf<EncodeHintType, Any>()
-        hints[EncodeHintType.MARGIN] = 1
-        val matrix: BitMatrix = MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, size, size, hints)
-        val width = matrix.width
-        val height = matrix.height
-        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        for (x in 0 until width) {
-            for (y in 0 until height) {
-                bitmap.setPixel(x, y, if (matrix[x, y]) Color.BLACK else Color.WHITE)
-            }
-        }
-        return bitmap
-    }
-}
+    <application
+        android:allowBackup="true"
+        android:icon="@mipmap/ic_launcher"
+        android:label="@string/app_name"
+        android:roundIcon="@mipmap/ic_launcher_round"
+        android:supportsRtl="true"
+        android:theme="@style/Theme.AppCompat.Light.NoActionBar">
+
+        <!-- כל ה-Activities -->
+        <activity android:name=".TechnicianActivity"/>
+        <activity android:name=".SupportActivity"/>
+        <activity android:name=".PackagesActivity"/>
+        <activity android:name=".NetworkCheckActivity"/>
+        <activity android:name=".BalanceActivity"/>
+
+        <!-- Main Activity -->
+        <activity
+            android:name=".MainActivity"
+            android:exported="true">
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN"/>
+                <category android:name="android.intent.category.LAUNCHER"/>
+            </intent-filter>
+        </activity>
+
+        <!-- ❌ הסרנו את SmsReceiver כדי למנוע קריסה -->
+
+    </application>
+
+</manifest>
