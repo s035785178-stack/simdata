4846848032ee679b11e65996f4e018dbe5f283e9
itai <s035785178@gmail.com>
2026-04-29 19:00:35 +0300

    stable working before required apps rebuild

--- app/src/main/java/com/stereotip/simdata/LauncherActivity.kt
+++ app/src/main/java/com/stereotip/simdata/LauncherActivity.kt
@@ -3,19 +3,14 @@ package com.stereotip.simdata
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.stereotip.simdata.util.RequiredApps
 
 class LauncherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (!RequiredApps.isRequiredDialerAvailable(this)) {
-            startActivity(Intent(this, RequiredAppsActivity::class.java))
-        } else {
-            startActivity(Intent(this, MainActivity::class.java))
-        }
-
+        // עוקף הכל - נכנס ישר לאפליקציה
+        startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
 }
